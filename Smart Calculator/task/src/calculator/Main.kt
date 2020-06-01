436cd70afdb4b2823607b8e6cb724075a7d1e688
david <david.m.willett@gmail.com>
2020-06-01 16:07:53 +0100

    Function main moved to start of Main file.

--- Smart Calculator/task/src/calculator/Main.kt
+++ Smart Calculator/task/src/calculator/Main.kt
@@ -4,6 +4,20 @@ import java.math.BigInteger
 
 fun String.isCommand() = this.first() == '/'
 
+fun main() {
+    var finished = false
+    do {
+        val input = readLine()!!
+        if (input.isEmpty()) continue
+        if (input.isCommand()) {
+            finished = Calculator.parseCommand(input)
+            continue
+        }
+        Calculator.parseStatement(input)
+    } while (!finished)
+    println("Bye!")
+}
+
 object Calculator {
     val variables = mutableMapOf<String, BigInteger>()
 
@@ -16,8 +30,12 @@ object Calculator {
     fun parseCommand(command: String): Boolean {
         return when (command) {
             "/exit" -> true
-            "/help" -> { println(HELP.trimIndent()); false }
-            else -> { println("Unknown command"); false }
+            "/help" -> {
+                println(HELP.trimIndent()); false
+            }
+            else -> {
+                println("Unknown command"); false
+            }
         }
     }
 
@@ -36,52 +54,42 @@ object Calculator {
             println("Unknown variable")
         }
     }
-}
 
-class Statement(text: String) {
-    private val tokens = Tokenizer.scan(text)
+    class Statement(text: String) {
+        private val tokens = Tokenizer.scan(text)
 
-    /**
-     * Executes the statement, returning an integer if the statement is an expression, otherwise null.
-     * Throws the appropriate exception if an error is detected.
-     */
-    fun execute(): BigInteger? {
-        return if (tokens.any { it is Token.Symbol && it.char == '=' }) {
-            val assignment = Assignment(tokens)
-            assignment.execute()
-            null
-        } else {
-            val expression = Expression(tokens)
-            expression.evaluate()
+        /**
+         * Executes this statement, returning an integer if the statement is an expression, otherwise null.
+         * Throws the appropriate exception if an error is detected.
+         */
+        fun execute(): BigInteger? {
+            return if (tokens.any { it is Token.Symbol && it.char == '=' }) {
+                val assignment = Assignment(tokens)
+                assignment.execute()
+                null
+            } else {
+                val expression = Expression(tokens)
+                expression.evaluate()
+            }
         }
     }
-}
 
-class Assignment(private val tokens: List<Token>) {
+    class Assignment(private val tokens: List<Token>) {
 
-    fun execute() {
-        if (tokens.size != 3) throw InvalidAssignmentException()
-        val identifier = tokens.component1()
-        if (identifier !is Token.Identifier) throw InvalidAssignmentException()
-        val assignment = tokens.component2()
-        if (assignment !is Token.Symbol || assignment.char != '=') throw InvalidAssignmentException()
-        val number = tokens.component3()
-        if (number !is Token.Integer) throw InvalidAssignmentException()
-        Calculator.variables[identifier.name] = number.value
-        return
-    }
-}
-
-fun main() {
-    var finished = false
-    do {
-        val input = readLine()!!
-        if (input.isEmpty()) continue
-        if (input.isCommand()) {
-            finished = Calculator.parseCommand(input)
-            continue
+        /**
+         * Executes this assignment. Throws an exception if the token list specified in the constructor was not of
+         * the form identifier = number, where whitespace is optional.
+         */
+        fun execute() {
+            if (tokens.size != 3) throw InvalidAssignmentException()
+            val identifier = tokens.component1()
+            if (identifier !is Token.Identifier) throw InvalidAssignmentException()
+            val assignment = tokens.component2()
+            if (assignment !is Token.Symbol || assignment.char != '=') throw InvalidAssignmentException()
+            val number = tokens.component3()
+            if (number !is Token.Integer) throw InvalidAssignmentException()
+            variables[identifier.name] = number.value
+            return
         }
-        Calculator.parseStatement(input)
-    } while (!finished)
-    println("Bye!")
+    }
 }
